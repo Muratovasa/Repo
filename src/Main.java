5409c4b46b8f8427246d425e9d76b3cf735f44d0
Muratova Svetlana <muratova.sa@phystech.edu>
2022-10-13 13:33:58 +0300

    улучшила структуру

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,12 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class Main {
-    public static void main(String[] args){
-        int elementsQueue=10;
-        int sleepTime= 1000;
-        int sleepTimeWork= 3000;
-        int countThread=61;
+    public static BlockingQueue<String> names = new ArrayBlockingQueue<>(Elements.elementsQueue);
 
-        BlockingQueue<String> names= new ArrayBlockingQueue<>(elementsQueue);
-        new Thread(()->{
-            for (int i=1; i<countThread;i++){
-                try {
-                    names.put("Звонок "+i);
-                    System.out.println("Поступил звонок "+i);
-                    Thread.sleep(sleepTime);
-                } catch (InterruptedException e) {
-                   return;
-                }
-            }
-        }).start();
-
-        new Thread(()->{
-            for (int i=1;i<countThread;i++){
-                try {
-                    System.out.println("Взяли "+names.take());
-                    Thread.sleep(sleepTimeWork);
-                } catch (InterruptedException e) {
-                    return;
-                }
-            }
-        }).start();
+    public static void main(String[] args) {
+        Thread treadCaller = new MyTreadCaller();
+        treadCaller.start();
+        Thread treadAnswered = new MyTreadAnswered();
+        treadAnswered.start();
     }
 }
